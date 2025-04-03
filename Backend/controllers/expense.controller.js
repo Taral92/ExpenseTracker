257ec448172a9dfe9d3daf3462bc0d74dd0fe900
Taral92 <taru.patel37225@gmail.com>
2025-04-03 16:33:19 +0530

    x

--- Backend/controllers/expense.controller.js
+++ Backend/controllers/expense.controller.js
@@ -1,8 +1,7 @@
-const expense = require('../models/expense.model');
-const expenseschema=require('../models/expense.model')
+const expenseschema = require("../models/expense.model");
 const addexpense = async (req, res) => {
   try {
-    const userId=req.userId
+    const userId = req.userId;
     const { description, amount, category } = req.body;
     if (!description || !amount || !category) {
       return res
@@ -13,28 +12,74 @@ const addexpense = async (req, res) => {
       description,
       amount,
       category,
-      userId
+      userId,
     });
-    return res.json({message:'your expense added',expense});
+    return res.json({ message: "your expense added", expense });
   } catch (error) {
     console.log(error);
-    
-    return res.json({ message: "error while adding your expenses"});
+
+    return res.json({ message: "error while adding your expenses" });
   }
-
 };
-const getallexpense=async (req,res)=>{
-   const userid=req.userId
-   const query={
-    userid //filter by id
-   }
-   const category=req.query.category || ''
-   const done=req.query.done || ''
+const getallexpense = async (req, res) => {
+  try {
+    const userid = req.userId;
+    const query = {
+      userid, //filter by id
+    };
+    const category = req.query.category || "";
+    const done = req.query.done || "";
 
-   if(category.toLowerCase() === 'all'){
-       // no need to filter
-   }else{
-      query.category={$regex:new RegExp(category,'i')}
+    if (category.toLowerCase() === "all") {
+      // no need to filter
+    } else {
+      query.category = { $regex: new RegExp(category, "i") };
+    }
+    if (done.toLowerCase() === "done") {
+      query.done = true;
+    } else if (done.toLowerCase() === "undone") {
+      query.done = false;
+    }
+    const expense = await expenseschema.find(query);
+    if (!expense || expense.length === 0) {
+      res.status(404).json({ message: "no expense found", success: false });
+    }
+    return res.ststus(200).json({ expense, success: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const markasdone = async (req, res) => {
+  const expenseid = req.params.id;
+  const done=req.body
+  const expense=await expenseschema.findByIdAndUpdate(expenseid,done,{new:true})
+  if(!expense){
+    return res.ststus(404).json({message:'expense not found',success:false})
+  }
+  return res.json({message:`expense mark as done ${expense.done? 'done': 'undone'}`,success:true})
+};
+const removeexpense=async (req,res)=>{
+         const expenseid=req.params.id
+         await expenseschema.findByIdAndDelete(expenseid)
+         return res.ststus(200).json({
+            message:'expense removed',
+            success:true
+         })
+}
+const updateexpense=async(req,res)=>{
+   try {
+      const {description,amount,category}=req.body
+       const expenseid=req.params.id
+       const updatedata={description,amount,category}
+       const expense=await expenseschema.findByIdAndUpdate(expenseid,updatedata,{new:true})
+       return res.ststus(200).json({
+        message:'expense updated',
+        expense,
+        success:true
+     })
+   } catch (error) {
+    console.log(error);
+    
    }
 }
-module.exports = addexpense;
+module.exports ={ addexpense,getallexpense,markasdone,removeexpense,updateexpense};
